f956f436bbdf39bdff4629ac5906f9eda64df10a
Marco Pereira <marco.10.pereira@gmail.com>
2026-03-07 14:57:58 +0000

    adicionado hr_val e resp_val

---
+++ js/stream_grid_animation.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#stream_grid_animation').scianimator({
+	    'images': ['images/stream_grid_animation1.png', 'images/stream_grid_animation2.png', 'images/stream_grid_animation3.png', 'images/stream_grid_animation4.png', 'images/stream_grid_animation5.png', 'images/stream_grid_animation6.png', 'images/stream_grid_animation7.png', 'images/stream_grid_animation8.png', 'images/stream_grid_animation9.png', 'images/stream_grid_animation10.png', 'images/stream_grid_animation11.png', 'images/stream_grid_animation12.png', 'images/stream_grid_animation13.png', 'images/stream_grid_animation14.png', 'images/stream_grid_animation15.png', 'images/stream_grid_animation16.png', 'images/stream_grid_animation17.png', 'images/stream_grid_animation18.png', 'images/stream_grid_animation19.png', 'images/stream_grid_animation20.png'],
+	    'width': 480,
+	    'delay': 200,
+	    'loopMode': 'loop'
+	});
+	$('#stream_grid_animation').scianimator('play');
+    });
+})(jQuery);
